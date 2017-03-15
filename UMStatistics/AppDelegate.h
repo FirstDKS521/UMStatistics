76e9d7ba3239fe598ad3973f09334efff95bd335
duks <duks@robooot.com>
2017-03-15 11:00:54 +0800

    Initial Commit

---
+++ UMStatistics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UMStatistics
+//
+//  Created by aDu on 2017/3/15.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UMStatistics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UMStatistics
+//
+//  Created by aDu on 2017/3/15.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
